9fb644ceffdd3f9bb34b9da795d42f60e95c59fc
Ella Moeller <emoellervon@icloud.com>
2020-05-19 16:43:12 +0200

    nearly created a whole app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,28 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(restart_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restart_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_19_105159) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
